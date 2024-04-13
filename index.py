e18b3470d75884e0a539c09c571a0c496a9a0469
Vaishnav Ghenge <vaishnavghenge@gmail.com>
2024-04-13 20:53:47 +0530

    Update: redis host

--- index.py
+++ index.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 CORS(app)
 
 app.config.update(
-    CELERY_BROKER_URL='redis://redis:6379/0',
-    CELERY_RESULT_BACKEND='redis://redis:6379/0',
+    CELERY_BROKER_URL='redis://localhost:6379/0',
+    CELERY_RESULT_BACKEND='redis://localhost:6379/0',
     TASK_IGNORE_RESULT=True,
 )
 celery_app = make_celery(app)

0b8c52e51deb3456671f6641a733186e2d446c25
Vaishnav Ghenge <vaishnavghenge@gmail.com>
2024-04-14 23:53:03 +0530

    Update: model to tiny

--- transcribe.py
+++ transcribe.py
@@ -12,7 +12,7 @@ def transcribe_with_whisper(resampled_data) -> str:
             str: Transcribed text.
     """
 
-    model = whisper.load_model("base")
+    model = whisper.load_model("tiny")
 
     result = model.transcribe(resampled_data, task="transcribe", language="en")
 

c1365bb7004f590d3bd704ae80a03c73e65f897a
Vaishnav Ghenge <vaishnavghenge@gmail.com>
2024-04-14 20:27:46 +0530

    update

--- transcribe.py
+++ transcribe.py
@@ -11,6 +11,8 @@ def transcribe_with_whisper(resampled_data) -> str:
         Returns:
             str: Transcribed text.
     """
+    pritn("initializing model")
+
     model = whisper.load_model("tiny")
     result = model.transcribe(resampled_data, task="transcribe")
 
